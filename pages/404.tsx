16a50edb31239b3e9c29361b53f401c28e2f6fc2
shrutiawa <shrutiawasthi220@gmail.com>
2024-07-24 17:37:28 +0530

    anchor tag remove

--- pages/404.tsx
+++ pages/404.tsx
@@ -20,7 +20,7 @@ const NotFound: React.FC = () => {
     <div className="not-found">
       <h1>404</h1>
       <h2>Oops! That page cannot be found :(</h2>
-      <p>Redirecting to <Link href="/"><a>Homepage</a></Link> for more marmite goodness...</p>
+      <p>Redirecting to <Link href="/">Homepage</Link> for more marmite goodness...</p>
       <style jsx>{`
         .not-found {
           background: #fff;
